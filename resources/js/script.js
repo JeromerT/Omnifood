540a321aaed25aa9f80d84e7fbcbcf9379d93a16
Unknown <jeroen.thomas@ymail.com>
2018-08-06 10:26:56 +0200

    animations and stuff

--- resources/js/script.js
+++ resources/js/script.js
@@ -42,7 +42,7 @@ $(document).ready(function() {
             event.preventDefault();
             $('html, body').animate({
               scrollTop: target.offset().top
-            }, 1000, function() {
+            }, 850, function() {
               // Callback after animation
               // Must change focus!
               var $target = $(target);
@@ -58,6 +58,31 @@ $(document).ready(function() {
         }
       });
     
+    
+    /* Animations on scroll */
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    },{
+       offset: '50%' 
+    });
+    
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    },{
+       offset: '50%' 
+    });
+    
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeInUp');
+    },{
+       offset: '50%' 
+    });
+    
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated fadeIn');
+    },{
+       offset: '50%' 
+    });
     
     
     
